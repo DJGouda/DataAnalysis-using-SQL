d4b66307f77f7cbde0bca8ca5a6477f2be62e2da
DJGouda <djgouda26@gmail.com>
2025-01-19 10:34:22 -0400

    Update sql_code.sql

--- sql_code.sql
+++ sql_code.sql
@@ -1,4 +1,4 @@
---find top 10 highest reveue generating products 
+--finding top 10 highest reveue generating products 
 select top 10 product_id,sum(sale_price) as sales
 from df_orders
 group by product_id
@@ -7,7 +7,7 @@ order by sales desc
 
 
 
---find top 5 highest selling products in each region
+--finding top 5 highest selling products in each region
 with cte as (
 select region,product_id,sum(sale_price) as sales
 from df_orders
@@ -20,13 +20,13 @@ where rn<=5
 
 
 
---find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
+--finding month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
 with cte as (
 select year(order_date) as order_year,month(order_date) as order_month,
 sum(sale_price) as sales
 from df_orders
 group by year(order_date),month(order_date)
---order by year(order_date),month(order_date)
+--ordering by year(order_date),month(order_date)
 	)
 select order_month
 , sum(case when order_year=2022 then sales else 0 end) as sales_2022
@@ -39,13 +39,13 @@ order by order_month
 
 
 
---for each category which month had highest sales 
+--finding for each category which month had highest sales 
 with cte as (
 select category,format(order_date,'yyyyMM') as order_year_month
 , sum(sale_price) as sales 
 from df_orders
 group by category,format(order_date,'yyyyMM')
---order by category,format(order_date,'yyyyMM')
+--ordering by category,format(order_date,'yyyyMM')
 )
 select * from (
 select *,
@@ -59,13 +59,13 @@ where rn=1
 
 
 
---which sub category had highest growth by profit in 2023 compare to 2022
+--finding the sub category had highest growth by profit in 2023 compare to 2022
 with cte as (
 select sub_category,year(order_date) as order_year,
 sum(sale_price) as sales
 from df_orders
 group by sub_category,year(order_date)
---order by year(order_date),month(order_date)
+--ordering by year(order_date),month(order_date)
 	)
 , cte2 as (
 select sub_category
